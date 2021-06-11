1e141e6752d4a1e5083a83af4680314c13e9d5b7
Akshat-Rana <akshat511716@gmail.com>
2021-06-11 18:57:16 +0530

    user input

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var buttonTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#text-input").value
 
 buttonTranslate.addEventListener("click", function(){
     
