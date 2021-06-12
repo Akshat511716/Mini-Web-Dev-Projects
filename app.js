2c6d03e684a45c4180d1b2d6146906e1554ac9c4
Akshat-Rana <akshat511716@gmail.com>
2021-06-12 09:59:36 +0530

    Added minion translation api

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+// API
+https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
 var buttonTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
 outputDiv.innerText = "Your banana translation will come here!";
 
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
 
 function clickHandler(){
-    outputDiv.innerText = textInput.value;
+    // outputDiv.innerText = textInput.value;
+    
+    var inputText = textInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText})
+    .catch(errorHandler => console.log("error occurred" + error));
+
     outputDiv.style.color = "black";
 }
 
